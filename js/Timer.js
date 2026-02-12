766b501ceefcb377cf38da463e102d6227d4d6b4
rainndev <182114243+rainndev@users.noreply.github.com>
2026-02-12 21:31:28 +0800

    feat: init project

---
+++ js/Timer.js
@@ -0,0 +1,10 @@
+export class Timer {
+  constructor(duration) {
+    this.duration = duration;
+    this.startTime = null;
+  }
+
+  start() {}
+  getElapsedTime() {}
+  stop() {}
+}
---
+++ js/TypingTest.js
@@ -0,0 +1,13 @@
+export class TypingTest {
+  constructor() {
+    this.timer = new Timer();
+    this.textGenerator = new TextGenerator();
+    this.stats = new Stats();
+    this.ui = new UI();
+  }
+
+  start() {}
+  handleInput(value) {}
+  finish() {}
+  reset() {}
+}
